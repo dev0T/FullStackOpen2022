88f404b07d1f3d3ecb3e94a3f10c4403e1b44487
Teryn Alves <terynalves@gmail.com>
2022-05-04 17:29:06 -0700

    [part1b] - 1-4 submission

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,25 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -29,9 +31,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1.name} exercises={props.part1.exercises} />
-      <Part part={props.part2.name} exercises={props.part2.exercises} />
-      <Part part={props.part3.name} exercises={props.part3.exercises} />
+      {props.parts.map((partObj) => {
+        return <Part part={partObj.name} exercises={partObj.exercises} />;
+      })}
     </div>
   );
 };
@@ -45,7 +47,15 @@ const Part = (props) => {
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>;
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts.reduce(
+        (previousValue, currentValue) => previousValue + currentValue.exercises,
+        0
+      )}
+    </p>
+  );
 };
 
 export default App;
