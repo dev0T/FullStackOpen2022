30f016a451c9804e6d8aa85806732ca84d9109f3
Teryn Alves <terynalves@gmail.com>
2022-05-06 19:17:24 -0700

    [1c] 1.8 submission

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,57 +1,5 @@
 import React, { useState } from "react";
 
-// Currying
-const handler = (set) => () => set((value) => value + 1);
-
-const sum = (...args) => {
-  return args.reduce(
-    (previousValue, currentValue) => previousValue + currentValue,
-    0
-  );
-};
-
-const avg = (weights, ...args) => {
-  const divisor = sum(...args);
-  const dividend = weights.reduce((accumulator, weight, index) => {
-    return accumulator + args[index] * weight;
-  }, 0);
-  return dividend / divisor;
-};
-
-const percentage = (...args) => {
-  const total = sum(...args);
-  const positive = args[0];
-  return (positive / total) * 10;
-};
-
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const weights = [1, 0, -1];
-
-  return (
-    <div>
-      <React.StrictMode>
-        <Header content={"Give feedback"} />
-        <Button handleClick={handler(setGood)} text={"Good"} />
-        <Button handleClick={handler(setNeutral)} text={"Neutral"} />
-        <Button handleClick={handler(setBad)} text={"Bad"} />
-        <Header content={"Statistics"} />
-        <Result text={"Good"} result={good} />
-        <Result text={"Neutral"} result={neutral} />
-        <Result text={"Bad"} result={bad} />
-        <Result text={"All"} result={sum(good, neutral, bad)} />
-        <Result text={"Average"} result={avg(weights, good, neutral, bad)} />
-        <Result
-          text={"Positive"}
-          result={`${percentage(good, neutral, bad)}%`}
-        />
-      </React.StrictMode>
-    </div>
-  );
-};
-
 const Header = ({ content }) => {
   return <h1>{content}</h1>;
 };
@@ -65,6 +13,72 @@ const Result = ({ text, result }) => {
     <p>
       {text} {result}
     </p>
+  );
+};
+
+const Statistics = ({ goodAmount, neutralAmount, badAmount }) => {
+  const weights = [1, 0, -1];
+  return (
+    <>
+      <Header content={"Statistics"} />
+      <Result text={"Good"} result={goodAmount} />
+      <Result text={"Neutral"} result={neutralAmount} />
+      <Result text={"Bad"} result={badAmount} />
+      <Result text={"All"} result={sum(goodAmount, neutralAmount, badAmount)} />
+      <Result
+        text={"Average"}
+        result={avg(weights, goodAmount, neutralAmount, badAmount)}
+      />
+      <Result
+        text={"Positive"}
+        result={`${percentage(goodAmount, neutralAmount, badAmount)}%`}
+      />
+    </>
+  );
+};
+
+// Currying
+const handler = (set) => () => set((value) => value + 1);
+
+const sum = (...args) => {
+  return args.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
+};
+
+
+const avg = (weights, ...args) => {
+  const divisor = sum(...args);
+  const dividend = weights.reduce((accumulator, weight, index) => {
+    return accumulator + args[index] * weight;
+  }, 0);
+  const result = dividend / (divisor ? divisor : 1);
+  return result;
+};
+
+const percentage = (...args) => {
+  const total = sum(...args);
+  const positive = args[0];
+  const result = (positive / (total ? total : 1)) * 10;
+  return result;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <React.StrictMode>
+        <Header content={"Give feedback"} />
+        <Button handleClick={handler(setGood)} text={"Good"} />
+        <Button handleClick={handler(setNeutral)} text={"Neutral"} />
+        <Button handleClick={handler(setBad)} text={"Bad"} />
+        <Statistics goodAmount={good} neutralAmount={neutral} badAmount={bad} />
+      </React.StrictMode>
+    </div>
   );
 };
 
