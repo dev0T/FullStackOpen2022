e63435e7b39fbb4863d447087f518a48369d814e
Teryn Alves <terynalves@gmail.com>
2022-05-14 11:21:50 -0700

    [2b] 2.6 submission

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,38 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
   const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => setNewName(event.target.value);
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+
+    setPersons((persons) =>
+      persons.concat({ name: newName, id: persons.length + 1 })
+    );
+
+    setNewName("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAdd}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => {
+        return <p key={person.id}>{person.name}</p>;
+      })}
     </div>
   );
-};
+};;
 
 export default App;
