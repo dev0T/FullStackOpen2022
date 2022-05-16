71050e059ddf87522b4e3f3bd03e3304f84a8610
Teryn Alves <terynalves@gmail.com>
2022-05-16 12:49:09 -0700

    [2b] 2.7 submission

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,23 +1,35 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567", id: 1 },
+  ]);
+  const [newName, setName] = useState("");
+  const [newNumber, setNumber] = useState("");
 
-  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleInputChange = (set) => (event) => set(event.target.value);
+
+  const clearInput = () => {
+    setName("");
+    setNumber("");
+  };
 
   const handleAdd = (event) => {
     event.preventDefault();
 
     if (findPerson(newName).length === 0) {
       setPersons((persons) =>
-        persons.concat({ name: newName, id: persons.length + 1 })
+        persons.concat({
+          name: newName,
+          number: newNumber,
+          id: persons.length + 1,
+        })
       );
     } else {
       alert(`${newName} is already added to phonebook`);
     }
 
-    setNewName("");
+    clearInput();
   };
 
   const findPerson = (name) => {
@@ -29,7 +41,11 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleAdd}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newName} onChange={handleInputChange(setName)} />
+        </div>
+        <div>
+          number:{" "}
+          <input value={newNumber} onChange={handleInputChange(setNumber)} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -37,10 +53,14 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => {
-        return <p key={person.id}>{person.name}</p>;
+        return (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        );
       })}
     </div>
   );
-};;
+};
 
 export default App;
