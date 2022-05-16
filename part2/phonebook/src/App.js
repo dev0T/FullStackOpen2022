3c1a776decfd2d1a466872f86c0606fce6a637e4
Teryn Alves <terynalves@gmail.com>
2022-05-16 14:50:14 -0700

    [2b] 2.9 submission

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567", id: 1 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setName] = useState("");
   const [newNumber, setNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState(persons);
 
   const handleInputChange = (set) => (event) => set(event.target.value);
 
@@ -32,6 +37,21 @@ const App = () => {
     clearInput();
   };
 
+  const handleSearch = (event) => {
+    const name = event.target.value;
+
+    const filter = filterPersonsByName(name);
+
+    setSearchName(name);
+    setFilteredPersons(filter);
+  };
+
+  const filterPersonsByName = (name) => {
+    return persons.filter((person) =>
+      person.name.toLowerCase().includes(name.toLowerCase())
+    );
+  };
+
   const findPerson = (name) => {
     return persons.filter((person) => person.name === name);
   };
@@ -39,6 +59,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        Filter shown with: <input value={searchName} onChange={handleSearch} />
+      </div>
+
+      <h2>Add a New</h2>
       <form onSubmit={handleAdd}>
         <div>
           name: <input value={newName} onChange={handleInputChange(setName)} />
@@ -52,7 +78,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
+      {filteredPersons.map((person) => {
         return (
           <p key={person.id}>
             {person.name} {person.number}
