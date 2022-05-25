708946a53b5733dffc440fee626b4c3323f7884b
Teryn Alves <terynalves@gmail.com>
2022-05-25 13:22:10 -0700

    [2c] 2.14 submission

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -23,7 +23,6 @@ function App() {
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      console.log(response);
       setCountries(response.data);
     });
   }, []);
--- part2/countries/src/CountryDetails.js
+++ part2/countries/src/CountryDetails.js
@@ -1,8 +1,14 @@
+import WeatherInfo from "./WeatherInfo";
+
 const CountryDetails = ({ country }) => {
-  const languages = Object.keys(country.languages);
-  const name = country.name.common;
-  const capital = country.capital;
-  const area = country.area;
+  const {
+    name: { common: name },
+    capital,
+    capitalInfo: { latlng },
+    languages,
+    area,
+  } = country;
+  const languagesKeys = Object.keys(languages);
 
   return (
     <>
@@ -11,11 +17,17 @@ const CountryDetails = ({ country }) => {
       <p>Area: {area}</p>
       <h4>Languages:</h4>
       <ul>
-        {languages.map((elementKey) => {
+        {languagesKeys.map((elementKey) => {
           return <li key={elementKey}>{country.languages[elementKey]}</li>;
         })}
       </ul>
-      <img src={country.flags.png} alt={`${name} flag`} width={200} />
+      <img
+        src={country.flags.png}
+        alt={`${name} flag`}
+        width={200}
+        style={{ border: "1px solid" }}
+      />
+      <WeatherInfo capital={capital} location={latlng} />
     </>
   );
 };
